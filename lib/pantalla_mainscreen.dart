3113526feb1cf1888638faf92f541dc451fe121e
JoseCev <escrita15@gmail.com>
2025-03-05 12:03:14 +0100

    Se han añadido el paquete de GoogleGEMini IA, y se ha añadido a la pantalla principal un pequeño chat de prueba

--- lib/pantalla_mainscreen.dart
+++ lib/pantalla_mainscreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+//import 'variables_globales.dart';
+import 'package:pet_plan_pruebas/src/widgets/custom_button.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 
 class PantallaPrincipal extends StatefulWidget {
   const PantallaPrincipal({super.key, required this.title});
@@ -12,11 +15,36 @@ class PantallaPrincipal extends StatefulWidget {
 class _PantallaPrincipalState extends State<PantallaPrincipal> {
 
   //AQUI VAN LAS VARIABLES GLOBALES//
+  final apiKey = 'AIzaSyBhCntUyCG397ROTUdrp8q7lNCKI5oLL8k';
+  late TextEditingController _campoPrompt;
+  String respuesta = "";
+
 
   @override
   void initState(){
     super.initState();
+    _campoPrompt = TextEditingController();
   }
+
+
+  void chatIA() async {
+
+    final model = GenerativeModel(
+      model: 'gemini-1.5-flash-latest',
+      apiKey: apiKey,
+    );
+
+    final prompt = _campoPrompt.text;
+    final content = [Content.text(prompt)];
+    final response = await model.generateContent(content);
+
+    
+    respuesta = response.text!.replaceAll(RegExp(r'\**'), '').toString();
+    print(response.text);
+    _campoPrompt.text = "";
+    setState(() {});
+  }
+
   
   @override
   Widget build(BuildContext context) {
@@ -34,6 +62,45 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
                 child: Image.asset("assets/logoLorena.png")
 
               ),
+
+              
+              Padding(
+                padding: const EdgeInsets.all(13),
+                child: TextField(  //Este es el campo de texto en el que se van introduciendo el correo del usuario 
+                  controller: _campoPrompt,  //Controlador para identificarlo
+                  decoration: const InputDecoration( 
+                    border: OutlineInputBorder(),
+                    labelText: "Introduce lo que quieras saber",
+                    labelStyle: TextStyle(fontSize: 18),
+                    ),
+                    
+                )
+              ),
+
+
+              Padding(padding: EdgeInsets.all(25),
+                child: Text("La respuesta es: $respuesta", style: TextStyle(fontSize: 20),)
+                ),
+              
+
+              Padding(padding: EdgeInsets.all(20)),
+              Container(
+                margin:EdgeInsets.only(left: 100, right: 100), //Esto lo separa del margen por la derecha y la izquierda
+                child:
+                  CustomButton(  //MI BOTON PRECIOSO para vosotros chat
+                    color: Color.fromARGB(0, 0, 0, 0),
+                    width: 100.0, //Ancho
+                    height: 30.0, //Alto
+                    callback: () {
+                      chatIA();
+                      
+                      
+                    },
+                    child: Text("Llamar a la IA", style: TextStyle(fontSize: 17, color: const Color.fromARGB(255, 0, 89, 255), fontStyle: FontStyle.normal)), //Aqui se podria poner una foto
+                  ),
+                ),
+              Padding(padding: EdgeInsets.all(20)),
+
              ] //Children  
         )
         )
