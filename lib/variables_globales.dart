1a3923f6d60fe3459823ce97960d517a435a1aee
JoseCev <escrita15@gmail.com>
2025-03-05 13:43:42 +0100

    varGlobalReworked

--- lib/variables_globales.dart
+++ lib/variables_globales.dart
@@ -1,5 +1,5 @@
 import 'constructor_registro.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
+
 
 class VariablesGlobales {
 
@@ -15,20 +15,3 @@ class VariablesGlobales {
 
 //API MODEL GEMINI
 
-class IaChatResponse{
-  void chatIA() async {
-
-  final apiKey = 'AIzaSyBhCntUyCG397ROTUdrp8q7lNCKI5oLL8k';
-
-  final model = GenerativeModel(
-    model: 'gemini-1.5-flash-latest',
-    apiKey: apiKey,
-  );
-
-  final prompt = 'What you want';
-  final content = [Content.text(prompt)];
-  final response = await model.generateContent(content);
-
-  print(response.text);
-  }
-}
