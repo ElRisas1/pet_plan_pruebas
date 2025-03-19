88b86967d038e461be3e58ef97b339c5b12c1f83
Cristian tecza <cristian_tecza_m-tsapp1ma2324@cev.com>
2025-03-19 13:38:54 +0100

    Supabase instalado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pet_plan_pruebas/pantalla_login.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+const supabaseUrl = 'https://fzukoqnipqclppkpotbc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6dWtvcW5pcHFjbHBwa3BvdGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4NzQ5ODAsImV4cCI6MjA1NTQ1MDk4MH0.Y7fmZFE3SiXvaSaYYNB1Y_WuWvAXNnA9Xdg0aJxEjjc';
 
 void main() {
   runApp(const MainApp());
