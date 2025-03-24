40bb269dc157cf5c064f7840c6079d5c8191945f
Lorena <lorena_miguel_perez_m-tsapp1ma2324@cev.com>
2025-03-24 20:13:32 +0100

    se reciben las imagenes de perfil de mascota y se ponen de forma adecuada.

--- lib/pantalla_mascota.dart
+++ lib/pantalla_mascota.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class PantallaMascota extends StatefulWidget {
+
+  // VARIABLES //
   final String nombreMascota;
+  final String imagenMascota;
 
   // Constructor
-  const PantallaMascota({super.key, required this.nombreMascota, required String title});
+  const PantallaMascota({super.key, required this.nombreMascota,required this.imagenMascota, required String title});
 
   @override
   _PantallaMascotaState createState() => _PantallaMascotaState();
@@ -27,50 +30,46 @@ class _PantallaMascotaState extends State<PantallaMascota> {
     return Stack(
       children: [
         // Appbar
-        AppBar(title: Text(widget.nombreMascota), 
-      ),
+        AppBar(title: Text(widget.nombreMascota),),
 
-      // TITULO DE LA PANTALLA
-      Padding(
-        padding: const EdgeInsets.only(top: 90, left: 160 ), // Ajusta el espacio superior para evitar que se solape con el AppBar
-        child: Text(
-               widget.nombreMascota,
-              style: const TextStyle(
-                fontSize: 30,
-                color: Color.fromARGB(218, 0, 0, 0),
-                fontWeight: FontWeight.bold, // Puedes cambiar el peso para que se vea más destacado
+        // TITULO DE LA PANTALLA
+        Padding(
+          padding: const EdgeInsets.only(top: 90, left: 170 ), // Ajusta el espacio superior para evitar que se solape con el AppBar
+          child: Text(
+                widget.nombreMascota,
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Color.fromARGB(218, 0, 0, 0),
+                  fontWeight: FontWeight.bold, // Puedes cambiar el peso para que se vea más destacado
+                ),
               ),
-            ),
-      ),
+        ),
       
        // IMAGEN DE PERFIL MASCOTA - CENTRADA
-      Positioned(
-        top: 150, 
-        left: 160, 
-        child: Material(
-          elevation: 8, // Agrega sombra al botón
-          shape: const CircleBorder(), // Mantiene la forma circular
-          clipBehavior: Clip.antiAlias, // Suaviza los bordes del círculo
+        Positioned(
+            top: 150, left: 160, 
+            child: Material(
+              elevation: 8, // Agrega sombra al botón
+              shape: const CircleBorder(), // Mantiene la forma circular
+              clipBehavior: Clip.antiAlias, // Suaviza los bordes del círculo
 
-          child: Container(
-            padding: const EdgeInsets.all(4), // Espaciado para el borde
-            decoration: BoxDecoration(
-              shape: BoxShape.circle, // Hace que el borde sea redondo
-              border: Border.all(color: Colors.white, width: 2.5), // Borde blanco
-            ),
-
-            child: InkWell(
-              splashColor: Colors.black26, // Efecto al tocar
-              // onTap
-              child: CircleAvatar( // Hace la imagen redonda
-              radius: 50,
-              backgroundImage: AssetImage('assets/profile_pic.png'),
+              child: Container(
+                padding: const EdgeInsets.all(4), // Espaciado para el borde
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle, // Hace que el borde sea redondo
+                  border: Border.all(color: Colors.white, width: 2.5), // Borde blanco
+                ),
+                child: InkWell(
+                  splashColor: Colors.black26, // Efecto al tocar
+                  // onTap
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(widget.imagenMascota), // Usa la imagen pasada
+                  ),
+                  ),
+                ),
               ),
-            ),
-          ),
         ),
-      ),
-
       ],//Children
     );
   }
