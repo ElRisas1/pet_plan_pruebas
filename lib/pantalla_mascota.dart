eea891c868ffe76953a42718377709491f67964d
Lorena <lorena_miguel_perez_m-tsapp1ma2324@cev.com>
2025-03-24 21:41:58 +0100

    Sceffold del widget build cambiado para que se cambie el color de la pantalla.

--- lib/pantalla_mascota.dart
+++ lib/pantalla_mascota.dart
@@ -12,8 +12,8 @@ class PantallaMascota extends StatefulWidget {
 
 
   // Constructor
-  const PantallaMascota({super.key, required this.nombreMascota, required this.imagenMascota, required this.recordatorios, // ✅ Se pasa como parámetro
-  });
+  const PantallaMascota({super.key, required this.nombreMascota, required this.imagenMascota, required this.recordatorios,});
+
   @override
   _PantallaMascotaState createState() => _PantallaMascotaState();
 }
@@ -23,20 +23,27 @@ class _PantallaMascotaState extends State<PantallaMascota> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(248, 144, 177, 235),
-      body: contenido(),
+      // Appbar
+      appBar: AppBar( 
+        title: Text(widget.nombreMascota),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255), // Color del AppBar
+      ),
+
+      body: Container(
+        width: double.infinity,  // Asegura que el Container cubra toda la pantalla
+        height: double.infinity, // Asegura que el Container cubra toda la pantalla
+        color: const Color.fromARGB(248, 144, 177, 235), // Fondo de la pantalla
+        child: contenido(),
+      ),
     );
   }
 
   Widget contenido(){
     return Stack(
       children: [
-        // Appbar
-        AppBar(title: Text(widget.nombreMascota),),
-
         // TITULO DE LA PANTALLA
         Padding(
-          padding: const EdgeInsets.only(top: 90, left: 170 ), // esto ajusta el espacio superior para evitar que se solape con el AppBar
+          padding: const EdgeInsets.only(top: 25, left: 168 ), // esto ajusta el espacio superior para evitar que se solape con el AppBar
           child: Text(
                 widget.nombreMascota,
                 style: const TextStyle(
@@ -49,7 +56,7 @@ class _PantallaMascotaState extends State<PantallaMascota> {
       
        // IMAGEN DE PERFIL MASCOTA - CENTRADA
         Positioned(
-            top: 150, left: 160, 
+            top: 100, left: 160, 
             child: Material(
               elevation: 8, 
               shape: const CircleBorder(), 
@@ -75,7 +82,7 @@ class _PantallaMascotaState extends State<PantallaMascota> {
 
         // TITULO INFORMACIÓN //
         Padding(
-          padding: const EdgeInsets.only(top: 300, left: 51),
+          padding: const EdgeInsets.only(top: 245, left: 51),
           child: Text(
             "Información de ${widget.nombreMascota}",
             style: const TextStyle(
@@ -88,7 +95,7 @@ class _PantallaMascotaState extends State<PantallaMascota> {
 
        // CAJA DE INFORMACIÓN DE LA MASCOTA //
         Positioned(
-          top: 350, left: 20, right: 20, // Ajusta la posición
+          top: 300, left: 36, right: 36, // Ajusta la posición
           child: Container(
             padding: const EdgeInsets.all(16),
             decoration: BoxDecoration(
@@ -111,7 +118,7 @@ class _PantallaMascotaState extends State<PantallaMascota> {
 
         // BOTON QR //
         Positioned(
-            top: 445, left: 100, right: 100,
+            top: 390, left: 100, right: 100,
             //boton
             child: ElevatedButton(
               onPressed: () {
@@ -142,7 +149,8 @@ class _PantallaMascotaState extends State<PantallaMascota> {
 
           // TITULO RECORDATORIOS //
           Padding(
-            padding: const EdgeInsets.only(top: 520, left: 115 ), // Ajusta el espacio superior para evitar que se solape con el AppBar
+            padding: const EdgeInsets.only(
+              top: 480, left: 115 ), // Ajusta el espacio superior para evitar que se solape con el AppBar
             child: const Text(
               "Recordatorios", 
               style: TextStyle(
@@ -155,7 +163,7 @@ class _PantallaMascotaState extends State<PantallaMascota> {
 
           // LISTA DE RECORDATORIOS //
           Positioned(
-            top: 580, left: 49, right: 49,
+            top: 530, left: 49, right: 49,
             child: Container(
               height: 200,
               width: 300,
@@ -178,6 +186,37 @@ class _PantallaMascotaState extends State<PantallaMascota> {
                 },
               ),
             ),
+          ),
+
+           // BOTON AGREGAR RECORDATORIO //
+          Positioned(
+              top: 750, left: 74, 
+              //boton
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PantallaQR(title: "pantalla qr", nombreMascota: '',)),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent, // Color del botón
+                  foregroundColor: Colors.white, // Color del texto
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // Bordes redondeados
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 45, 
+                    vertical: 20), // Tamaño del botón
+                  elevation: 8, // Sombra para el botón
+                ),
+                child: const Text(
+                  "Nuevo Recordatorio",
+                  style: TextStyle(
+                    fontSize: 20, 
+                    fontWeight: FontWeight.bold),
+                ),
+              ),
           ),
       ],
     );
