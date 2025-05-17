b01a00f2f26302f0d21023dc34268fa2b84954a4
JoseCasa <escrita15@gmail.com>
2025-05-17 19:46:30 +0200

    Añadido el paquete de shared preferences y su punto de guardado

--- lib/variables_globales.dart
+++ lib/variables_globales.dart
@@ -1,5 +1,6 @@
 import 'constructor_registro.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class VariablesGlobales {
 
@@ -21,6 +22,28 @@ class Mensaje {
     required this.isSentByMe,
   });
 }
+
+//Variable que guarda en local:
+final SharedPreferencesAsync asyncPrefs = SharedPreferencesAsync();
+
+//Aqui se guardarán cosas:
+
+class SharedPreferencesDemoState {
+  final Future<SharedPreferencesWithCache> _prefs =
+      SharedPreferencesWithCache.create(
+          cacheOptions: const SharedPreferencesWithCacheOptions(
+              // This cache will only accept the key 'counter'.
+              allowList: <String>{'counter'},));
+  late Future<int> _counter; //Lo de Future es que es una variable incompleta pero que se llenará en algun momento
+}
+
+
+
+
+
+
+
+
 
 
 //API MODEL GEMINI
